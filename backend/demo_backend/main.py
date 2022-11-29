ea35d9bb20faa04003c465f39721527070177cac
Brent Stone <brent.j.stone2.mil@mail.mil>
2022-11-29 17:25:49 -0500

    AVENGERCON Initial Setup final draft
    Working initial setup for testing all key elements of the docker compose demo run on a machine.

--- backend/demo_backend/main.py
+++ backend/demo_backend/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 
 
 def get_application() -> FastAPI:
-    _app = FastAPI(title="Hello, FastPAI!", version="0.1.0")
+    _app = FastAPI(title="Hello, FastAPI!", version="0.1.0")
 
     return _app
 
