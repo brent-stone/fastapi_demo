28cc0118aca7353ec2349cac5f5e08702bd25a65
Brent Stone <brent@stoneguardsoftware.com>
2022-11-29 20:42:50 -0500

    1. Stub out directory structure
    First let's stub out a directory structure

--- backend/demo_backend/main.py
+++ backend/demo_backend/main.py
@@ -1,10 +1,13 @@
 """
-Primary FastAPI startup
+Primary FastAPI application instance creation
 """
 from fastapi import FastAPI
 
 
 def get_application() -> FastAPI:
+    """
+    FastAPI application creation entrypoint
+    """
     _app = FastAPI(title="Hello, FastAPI!", version="0.1.0")
 
     return _app
